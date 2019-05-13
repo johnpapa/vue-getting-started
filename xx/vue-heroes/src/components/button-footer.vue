833f1bdc20009cc98de314befdb24ccec3c52bc6
John Papa <john@johnpapa.net>
2019-05-13 15:53:00 -0400

    tweak

--- xx/vue-heroes/src/components/button-footer.vue
+++ xx/vue-heroes/src/components/button-footer.vue
@@ -3,12 +3,13 @@
     class="link card-footer-item"
     :class="className"
     :aria-label="label"
-    tabIndex="0"
+    tabindex="0"
     @click="handleClick"
     :data-index="dataIndex"
     :data-id="dataId"
   >
-    <i :class="iconClasses"></i> <span>{{ label }}</span>
+    <i :class="iconClasses"></i>
+    <span>{{ label }}</span>
   </button>
 </template>
 
