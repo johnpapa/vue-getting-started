cc2d65d6a5fa1ad5ce63996e3f086fa981b779c9
John Papa <john@johnpapa.net>
2019-05-22 11:40:35 -0400

    added more form elements

--- 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -3,7 +3,7 @@
     <div class="section content-title-group">
       <h2 class="title">Heroes</h2>
     </div>
-    <div class="columns is-multiline is-variable">
+    <div class="columns">
       <div class="column is-8">
         <div class="card edit-detail">
           <header class="card-header">
@@ -13,30 +13,85 @@
             <div class="content">
               <div class="field">
                 <label class="label" for="id">id</label>
-                <label class="input" name="id" readonly>{{ hero.id }}</label>
+                <label class="input" id="id" readonly>{{ hero.id }}</label>
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
                 <input
                   class="input"
-                  name="firstName"
+                  id="firstName"
                   type="text"
                   v-model="hero.firstName"
                 />
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" name="lastName" v-model="hero.lastName" />
+                <input class="input" id="lastName" v-model="hero.lastName" />
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <input
+                <textarea
                   class="input"
-                  name="description"
+                  id="description"
                   type="text"
                   v-model="hero.description"
                 />
               </div>
+              <div class="field">
+                <label class="radio" for="color-red">
+                  <input
+                    type="radio"
+                    id="color-red"
+                    value="red"
+                    v-model="hero.color"
+                  />
+                  red
+                </label>
+                <label class="radio" for="color-blue">
+                  <input
+                    type="radio"
+                    id="color-blue"
+                    value="blue"
+                    v-model="hero.color"
+                  />
+                  blue
+                </label>
+                <label class="radio" for="color-green">
+                  <input
+                    type="radio"
+                    id="color-green"
+                    value="green"
+                    v-model="hero.color"
+                  />
+                  green
+                </label>
+              </div>
+              <div class="field select">
+                <label class="checkbox" for="active">
+                  super power
+                  <div class="select is-primary">
+                    <select v-model="hero.superPower">
+                      <option disabled value>Please select one</option>
+                      <option>Speed</option>
+                      <option>Flight</option>
+                      <option>Strength</option>
+                      <option>Invisibility</option>
+                    </select>
+                  </div>
+                </label>
+              </div>
+              <div class="field">
+                <label class="checkbox" for="active">
+                  active
+                  <input
+                    type="checkbox"
+                    class="is-primary"
+                    id="active"
+                    v-model="hero.active"
+                  />
+                </label>
+              </div>
+              <pre>{{ hero | json }}</pre>
             </div>
           </div>
           <footer class="card-footer">
@@ -71,6 +126,9 @@ export default {
         firstName: 'Madelyn',
         lastName: 'Papa',
         description: 'the cat whisperer',
+        capeColor: '',
+        superPower: '',
+        active: true,
       },
     };
   },
