c037d2cb1c4cc77427e0e2ddde439b4471e1086b
John Papa <john@johnpapa.net>
2019-06-15 19:34:19 -0400

    init the begin with the end of 04

---
+++ 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="content-container">
+    <div class="section content-title-group">
+      <h2 class="title">Heroes</h2>
+    </div>
+    <div class="columns">
+      <div class="column is-3">
+        <header class="card-header">
+          <p class="card-header-title">heroes list</p>
+        </header>
+        <ul class="list is-hoverable">
+          <li v-for="hero in heroes" :key="hero.id">
+            <a
+              class="list-item"
+              :class="{ 'is-active': selectedHero === hero }"
+              @click="selectedHero = hero"
+            >
+              <span>{{ hero.firstName }}</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="columns" v-if="selectedHero">
+      <div class="column is-3">
+        <header class="card-header">
+          <p class="card-header-title">{{ selectedHero.firstName }}</p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <div class="field">
+              <label class="label" for="id">id</label>
+              <label class="input" id="id" readonly>
+                {{ selectedHero.id }}
+              </label>
+            </div>
+            <div class="field">
+              <label class="label" for="firstName">first name</label>
+              <input
+                class="input"
+                id="firstName"
+                v-model="selectedHero.firstName"
+              />
+            </div>
+            <div class="field">
+              <label class="checkbox" for="show">
+                show more
+                <input
+                  type="checkbox"
+                  class="is-primary"
+                  id="show"
+                  v-model="showMore"
+                />
+              </label>
+            </div>
+            <div class="field" v-show="showMore">
+              <label class="label" for="lastName">last name</label>
+              <input
+                class="input"
+                id="lastName"
+                v-model="selectedHero.lastName"
+              />
+            </div>
+            <div class="field" v-show="showMore">
+              <label class="label" for="description">description</label>
+              <input
+                class="input"
+                id="description"
+                v-model="selectedHero.description"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Heroes',
+  data() {
+    return {
+      selectedHero: undefined,
+      showMore: false,
+      heroes: [
+        {
+          id: 10,
+          firstName: 'Ella',
+          lastName: 'Papa',
+          description: 'fashionista',
+        },
+        {
+          id: 20,
+          firstName: 'Madelyn',
+          lastName: 'Papa',
+          description: 'the cat whisperer',
+        },
+        {
+          id: 30,
+          firstName: 'Haley',
+          lastName: 'Papa',
+          description: 'pen wielder',
+        },
+      ],
+    };
+  },
+};
+</script>
