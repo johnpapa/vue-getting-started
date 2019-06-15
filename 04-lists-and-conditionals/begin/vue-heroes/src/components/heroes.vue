c2fdc3674d01b9e3b49f06c0bbea75884e325e24
John Papa <john@johnpapa.net>
2019-06-15 19:46:40 -0400

    init the begin code for 04

--- 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
+++ 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
@@ -8,20 +8,9 @@
         <header class="card-header">
           <p class="card-header-title">heroes list</p>
         </header>
-        <ul class="list is-hoverable">
-          <li v-for="hero in heroes" :key="hero.id">
-            <a
-              class="list-item"
-              :class="{ 'is-active': selectedHero === hero }"
-              @click="selectedHero = hero"
-            >
-              <span>{{ hero.firstName }}</span>
-            </a>
-          </li>
-        </ul>
       </div>
     </div>
-    <div class="columns" v-if="selectedHero">
+    <div class="columns">
       <div class="column is-3">
         <header class="card-header">
           <p class="card-header-title">{{ selectedHero.firstName }}</p>
@@ -43,17 +32,6 @@
               />
             </div>
             <div class="field">
-              <label class="checkbox" for="show">
-                show more
-                <input
-                  type="checkbox"
-                  class="is-primary"
-                  id="show"
-                  v-model="showMore"
-                />
-              </label>
-            </div>
-            <div class="field" v-show="showMore">
               <label class="label" for="lastName">last name</label>
               <input
                 class="input"
@@ -61,7 +39,7 @@
                 v-model="selectedHero.lastName"
               />
             </div>
-            <div class="field" v-show="showMore">
+            <div class="field">
               <label class="label" for="description">description</label>
               <input
                 class="input"
@@ -81,8 +59,12 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      selectedHero: undefined,
-      showMore: false,
+      selectedHero: {
+        id: 20,
+        firstName: 'Madelyn',
+        lastName: 'Papa',
+        description: 'the cat whisperer',
+      },
       heroes: [
         {
           id: 10,
