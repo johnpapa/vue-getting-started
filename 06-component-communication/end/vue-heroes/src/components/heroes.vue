288257ff8f0834ba9efd000d5125154ae25d7b08
John Papa <john@johnpapa.net>
2019-07-12 20:41:05 -0400

    moved data to module in 06-end

--- 06-component-communication/end/vue-heroes/src/components/heroes.vue
+++ 06-component-communication/end/vue-heroes/src/components/heroes.vue
@@ -41,45 +41,9 @@
 </template>
 
 <script>
-import { format } from 'date-fns';
-
-import { inputDateFormat, heroWatchers, lifecycleHooks } from '../shared';
+import { heroWatchers, lifecycleHooks, ourHeroes } from '../shared';
 import HeroDetail from '@/components/hero-detail';
 
-const ourHeroes = [
-  {
-    id: 10,
-    firstName: 'Ella',
-    lastName: 'Papa',
-    capeCounter: 1,
-    originDate: format(new Date(1996, 5, 1), inputDateFormat),
-    description: 'fashionista',
-  },
-  {
-    id: 20,
-    firstName: 'Madelyn',
-    lastName: 'Papa',
-    capeCounter: 3,
-    originDate: format(new Date(1998, 7, 1), inputDateFormat),
-    description: 'the cat whisperer',
-  },
-  {
-    id: 30,
-    firstName: 'Haley',
-    lastName: 'Papa',
-    capeCounter: 2,
-    originDate: format(new Date(1999, 8, 1), inputDateFormat),
-    description: 'pen wielder',
-  },
-  {
-    id: 40,
-    firstName: 'Landon',
-    lastName: 'Papa',
-    capeCounter: 0,
-    originDate: format(new Date(2000, 9, 1), inputDateFormat),
-    description: 'arc trooper',
-  },
-];
 export default {
   name: 'Heroes',
   data() {
