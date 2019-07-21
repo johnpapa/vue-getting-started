b5ecab33ba8970a5738490db2e91974105c25055
John Papa <john@johnpapa.net>
2019-07-21 14:48:03 -0400

    fixing the data

--- 08-routing/end/vue-heroes/vue.config.js
+++ 08-routing/end/vue-heroes/vue.config.js
@@ -5,9 +5,9 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
-        ws: true,
-        changeOrigin: true,
+        target: 'http://localhost:8888', // target host
+        ws: true, // proxy websockets
+        changeOrigin: true, // needed for virtual hosted sites
       },
     },
   },
