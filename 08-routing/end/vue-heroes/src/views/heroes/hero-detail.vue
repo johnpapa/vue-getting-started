5b74d71e037ae06ac80f0aace5b7e793de9ae495
John Papa <john@johnpapa.net>
2019-07-21 13:21:52 -0400

    title for details pages

--- 08-routing/end/vue-heroes/src/views/heroes/hero-detail.vue
+++ 08-routing/end/vue-heroes/src/views/heroes/hero-detail.vue
@@ -1,46 +1,49 @@
 <template>
   <div>
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">{{ fullName }}</p>
-      </header>
-      <div class="card-content">
-        <div class="content">
-          <div class="field">
-            <label class="label" for="id">id</label>
-            <label class="input" name="id" readonly>{{ hero.id }}</label>
-          </div>
-          <div class="field">
-            <label class="label" for="firstName">first name</label>
-            <input class="input" name="firstName" v-model="hero.firstName" />
-          </div>
-          <div class="field">
-            <label class="label" for="lastName">last name</label>
-            <input class="input" name="lastName" v-model="hero.lastName" />
-          </div>
-          <div class="field">
-            <label class="label" for="description">description</label>
-            <input
-              class="input"
-              name="description"
-              v-model="hero.description"
-            />
+    <div class="section content-title-group">
+      <h2 class="title">Edit Hero</h2>
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">{{ fullName }}</p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <div class="field">
+              <label class="label" for="id">id</label>
+              <label class="input" name="id" readonly>{{ hero.id }}</label>
+            </div>
+            <div class="field">
+              <label class="label" for="firstName">first name</label>
+              <input class="input" name="firstName" v-model="hero.firstName" />
+            </div>
+            <div class="field">
+              <label class="label" for="lastName">last name</label>
+              <input class="input" name="lastName" v-model="hero.lastName" />
+            </div>
+            <div class="field">
+              <label class="label" for="description">description</label>
+              <input
+                class="input"
+                name="description"
+                v-model="hero.description"
+              />
+            </div>
           </div>
         </div>
+        <footer class="card-footer">
+          <button
+            class="link card-footer-item cancel-button"
+            @click="cancelHero()"
+          >
+            <i class="fas fa-undo"></i>
+            <span>Cancel</span>
+          </button>
+          <button class="link card-footer-item" @click="saveHero()">
+            <i class="fas fa-save"></i>
+            <span>Save</span>
+          </button>
+        </footer>
       </div>
-      <footer class="card-footer">
-        <button
-          class="link card-footer-item cancel-button"
-          @click="cancelHero()"
-        >
-          <i class="fas fa-undo"></i>
-          <span>Cancel</span>
-        </button>
-        <button class="link card-footer-item" @click="saveHero()">
-          <i class="fas fa-save"></i>
-          <span>Save</span>
-        </button>
-      </footer>
     </div>
   </div>
 </template>
--- 08-routing/end/vue-heroes/src/views/villains/villain-detail.vue
+++ 08-routing/end/vue-heroes/src/views/villains/villain-detail.vue
@@ -1,46 +1,53 @@
 <template>
   <div>
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">{{ fullName }}</p>
-      </header>
-      <div class="card-content">
-        <div class="content">
-          <div class="field">
-            <label class="label" for="id">id</label>
-            <label class="input" name="id" readonly>{{ villain.id }}</label>
-          </div>
-          <div class="field">
-            <label class="label" for="firstName">first name</label>
-            <input class="input" name="firstName" v-model="villain.firstName" />
-          </div>
-          <div class="field">
-            <label class="label" for="lastName">last name</label>
-            <input class="input" name="lastName" v-model="villain.lastName" />
-          </div>
-          <div class="field">
-            <label class="label" for="description">description</label>
-            <input
-              class="input"
-              name="description"
-              v-model="villain.description"
-            />
+    <div class="section content-title-group">
+      <h2 class="title">Edit Villain</h2>
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">{{ fullName }}</p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <div class="field">
+              <label class="label" for="id">id</label>
+              <label class="input" name="id" readonly>{{ villain.id }}</label>
+            </div>
+            <div class="field">
+              <label class="label" for="firstName">first name</label>
+              <input
+                class="input"
+                name="firstName"
+                v-model="villain.firstName"
+              />
+            </div>
+            <div class="field">
+              <label class="label" for="lastName">last name</label>
+              <input class="input" name="lastName" v-model="villain.lastName" />
+            </div>
+            <div class="field">
+              <label class="label" for="description">description</label>
+              <input
+                class="input"
+                name="description"
+                v-model="villain.description"
+              />
+            </div>
           </div>
         </div>
+        <footer class="card-footer">
+          <button
+            class="link card-footer-item cancel-button"
+            @click="cancelVillain()"
+          >
+            <i class="fas fa-undo"></i>
+            <span>Cancel</span>
+          </button>
+          <button class="link card-footer-item" @click="saveVillain()">
+            <i class="fas fa-save"></i>
+            <span>Save</span>
+          </button>
+        </footer>
       </div>
-      <footer class="card-footer">
-        <button
-          class="link card-footer-item cancel-button"
-          @click="cancelVillain()"
-        >
-          <i class="fas fa-undo"></i>
-          <span>Cancel</span>
-        </button>
-        <button class="link card-footer-item" @click="saveVillain()">
-          <i class="fas fa-save"></i>
-          <span>Save</span>
-        </button>
-      </footer>
     </div>
   </div>
 </template>
