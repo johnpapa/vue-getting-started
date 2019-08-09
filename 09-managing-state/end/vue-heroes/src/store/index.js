2102251d86e2fa14ef705502fe0ecce9cdce6308
John Papa <john@johnpapa.net>
2019-08-09 10:48:57 -0400

    added villains to the store

--- 09-managing-state/end/vue-heroes/src/store/index.js
+++ 09-managing-state/end/vue-heroes/src/store/index.js
@@ -7,6 +7,10 @@ import {
   DELETE_HERO,
   GET_HEROES,
   UPDATE_HERO,
+  ADD_VILLAIN,
+  DELETE_VILLAIN,
+  GET_VILLAINS,
+  UPDATE_VILLAIN,
 } from './mutation-types';
 
 // const captains = console;
@@ -16,6 +20,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     heroes: [],
+    villains: [],
   },
   mutations: {
     [ADD_HERO](state, hero) {
@@ -31,6 +36,20 @@ export default new Vuex.Store({
     },
     [DELETE_HERO](state, hero) {
       state.heroes = [...state.heroes.filter(p => p.id !== hero.id)];
+    },
+    [ADD_VILLAIN](state, villain) {
+      state.villains.unshift(villain); // mutable addition
+    },
+    [UPDATE_VILLAIN](state, villain) {
+      const index = state.villains.findIndex(h => h.id === villain.id);
+      state.villains.splice(index, 1, villain);
+      state.villains = [...state.villains];
+    },
+    [GET_VILLAINS](state, villains) {
+      state.villains = villains;
+    },
+    [DELETE_VILLAIN](state, villain) {
+      state.villains = [...state.villains.filter(p => p.id !== villain.id)];
     },
   },
   actions: {
@@ -65,9 +84,27 @@ export default new Vuex.Store({
       //   return updatedHero;
       // });
     },
+    async addVillainAction({ commit }, villain) {
+      const addedVillain = await dataService.addVillain(villain);
+      commit(ADD_VILLAIN, addedVillain);
+    },
+    async deleteVillainAction({ commit }, villain) {
+      const deletedVillainId = await dataService.deleteVillain(villain);
+      commit(DELETE_VILLAIN, deletedVillainId);
+    },
+    async getVillainsAction({ commit }) {
+      const villains = await dataService.getVillains();
+      commit(GET_VILLAINS, villains);
+    },
+    async updateVillainAction({ commit }, villain) {
+      const updatedVillain = await dataService.updateVillain(villain);
+      commit(UPDATE_VILLAIN, updatedVillain);
+    },
   },
   getters: {
     heroes: state => state.heroes,
     getHeroById: state => id => state.heroes.find(h => h.id === id),
+    villains: state => state.villains,
+    getVillainById: state => id => state.villains.find(v => v.id === id),
   },
 });
