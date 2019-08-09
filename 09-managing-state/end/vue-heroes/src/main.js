23dbbe6712f3836e3ebb14451456a75475e85736
John Papa <john@johnpapa.net>
2019-08-09 10:24:10 -0400

    vuex in main

--- 09-managing-state/end/vue-heroes/src/main.js
+++ 09-managing-state/end/vue-heroes/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from '@/app';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
