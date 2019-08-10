5c86bc064a922a699df977e837de2b6579b77cfb
John Papa <john@johnpapa.net>
2019-08-10 16:43:18 -0400

    updating the store

--- xx-final/vue-heroes/src/store/index.js
+++ xx-final/vue-heroes/src/store/index.js
@@ -15,80 +15,82 @@ import {
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    heroes: [],
-    villains: [],
+const state = {
+  heroes: [],
+  villains: [],
+};
+
+const mutations = {
+  [ADD_HERO](state, hero) {
+    state.heroes.unshift(hero); // mutable addition
   },
-  mutations: {
-    [ADD_HERO](state, hero) {
-      state.heroes.unshift(hero); // mutable addition
-    },
-    [UPDATE_HERO](state, hero) {
-      const index = state.heroes.findIndex(h => h.id === hero.id);
-      state.heroes.splice(index, 1, hero);
-      state.heroes = [...state.heroes];
-    },
-    [GET_HEROES](state, heroes) {
-      state.heroes = heroes;
-    },
-    [DELETE_HERO](state, hero) {
-      state.heroes = [...state.heroes.filter(p => p.id !== hero.id)];
-    },
-    [ADD_VILLAIN](state, villain) {
-      state.villains.unshift(villain); // mutable addition
-    },
-    [UPDATE_VILLAIN](state, villain) {
-      const index = state.villains.findIndex(h => h.id === villain.id);
-      state.villains.splice(index, 1, villain);
-      state.villains = [...state.villains];
-    },
-    [GET_VILLAINS](state, villains) {
-      state.villains = villains;
-    },
-    [DELETE_VILLAIN](state, villain) {
-      state.villains = [...state.villains.filter(p => p.id !== villain.id)];
-    },
+  [UPDATE_HERO](state, hero) {
+    const index = state.heroes.findIndex(h => h.id === hero.id);
+    state.heroes.splice(index, 1, hero);
+    state.heroes = [...state.heroes];
   },
-  actions: {
-    // actions let us get to ({ state, getters, commit, dispatch }) {
-    async addHeroAction({ commit }, hero) {
-      const addedHero = await dataService.addHero(hero);
-      commit(ADD_HERO, addedHero);
-    },
-    async deleteHeroAction({ commit }, hero) {
-      const deletedHeroId = await dataService.deleteHero(hero);
-      commit(DELETE_HERO, deletedHeroId);
-    },
-    async getHeroesAction({ commit }) {
-      const heroes = await dataService.getHeroes();
-      commit(GET_HEROES, heroes);
-    },
-    async updateHeroAction({ commit }, hero) {
-      const updatedHero = await dataService.updateHero(hero);
-      commit(UPDATE_HERO, updatedHero);
-    },
-    async addVillainAction({ commit }, villain) {
-      const addedVillain = await dataService.addVillain(villain);
-      commit(ADD_VILLAIN, addedVillain);
-    },
-    async deleteVillainAction({ commit }, villain) {
-      const deletedVillainId = await dataService.deleteVillain(villain);
-      commit(DELETE_VILLAIN, deletedVillainId);
-    },
-    async getVillainsAction({ commit }) {
-      const villains = await dataService.getVillains();
-      commit(GET_VILLAINS, villains);
-    },
-    async updateVillainAction({ commit }, villain) {
-      const updatedVillain = await dataService.updateVillain(villain);
-      commit(UPDATE_VILLAIN, updatedVillain);
-    },
+  [GET_HEROES](state, heroes) {
+    state.heroes = heroes;
   },
-  getters: {
-    heroes: state => state.heroes,
-    getHeroById: state => id => state.heroes.find(h => h.id === id),
-    villains: state => state.villains,
-    getVillainById: state => id => state.villains.find(v => v.id === id),
+  [DELETE_HERO](state, hero) {
+    state.heroes = [...state.heroes.filter(p => p.id !== hero.id)];
   },
-});
+  [ADD_VILLAIN](state, villain) {
+    state.villains.unshift(villain); // mutable addition
+  },
+  [UPDATE_VILLAIN](state, villain) {
+    const index = state.villains.findIndex(h => h.id === villain.id);
+    state.villains.splice(index, 1, villain);
+    state.villains = [...state.villains];
+  },
+  [GET_VILLAINS](state, villains) {
+    state.villains = villains;
+  },
+  [DELETE_VILLAIN](state, villain) {
+    state.villains = [...state.villains.filter(p => p.id !== villain.id)];
+  },
+};
+
+const actions = {
+  // actions let us get to ({ state, getters, commit, dispatch }) {
+  async addHeroAction({ commit }, hero) {
+    const addedHero = await dataService.addHero(hero);
+    commit(ADD_HERO, addedHero);
+  },
+  async deleteHeroAction({ commit }, hero) {
+    const deletedHeroId = await dataService.deleteHero(hero);
+    commit(DELETE_HERO, deletedHeroId);
+  },
+  async getHeroesAction({ commit }) {
+    const heroes = await dataService.getHeroes();
+    commit(GET_HEROES, heroes);
+  },
+  async updateHeroAction({ commit }, hero) {
+    const updatedHero = await dataService.updateHero(hero);
+    commit(UPDATE_HERO, updatedHero);
+  },
+  async addVillainAction({ commit }, villain) {
+    const addedVillain = await dataService.addVillain(villain);
+    commit(ADD_VILLAIN, addedVillain);
+  },
+  async deleteVillainAction({ commit }, villain) {
+    const deletedVillainId = await dataService.deleteVillain(villain);
+    commit(DELETE_VILLAIN, deletedVillainId);
+  },
+  async getVillainsAction({ commit }) {
+    const villains = await dataService.getVillains();
+    commit(GET_VILLAINS, villains);
+  },
+  async updateVillainAction({ commit }, villain) {
+    const updatedVillain = await dataService.updateVillain(villain);
+    commit(UPDATE_VILLAIN, updatedVillain);
+  },
+};
+
+const getters = {
+  // parameterized getters are not cached. so this is just a convenience to get the state.
+  getHeroById: state => id => state.heroes.find(h => h.id === id),
+  getVillainById: state => id => state.villains.find(v => v.id === id),
+};
+
+export default new Vuex.Store({ state, mutations, actions, getters });
